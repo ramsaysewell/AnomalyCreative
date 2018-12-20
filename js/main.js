ae7aab84ff76dc40a4f176099ae981e1cc47d3c7
Ramsay Sewell <sewellrf@gmail.com>
2018-12-20 20:33:35 +0000

    showcase initial commit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
 $("span").arctext({
   radius: 4000,
-  dir: 1,
-  rotate: true
-})
+  dir: 1
+});
+
+// $('.showcase *').mouseover(function() {
+//   $(this).find("h1").css("opacity", 1);
+// })
+// $('.item').mouseout(function() {
+//   $(this).find("h1").css("opacity", 0);
+// })
+
+// $(".item").mouseover(function() {
+//   $(this).find("img").css("opacity", 0.5);
+//   $(this).find(".itemText").css("opacity", 1)
+//
+// });
+// $(".item").mouseout(function() {
+//   $(this).find("img").css("opacity", 1);
+//   $(this).find(".itemText").css("opacity", 0)
+// });
