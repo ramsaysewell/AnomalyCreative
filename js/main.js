2ad0795515177f3ccabff7434808e29cf8c79e0c
Ramsay Sewell <sewellrf@gmail.com>
2018-12-22 01:21:46 +0000

    Updated Contact form to use Flexbox over Bootstrap

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 $(".item").mouseover(function() {
   $(this).find(".overlay").css("opacity", 0.8);
   $(this).find(".itemText").css("opacity", 1);
@@ -8,3 +7,26 @@ $(".item").mouseout(function() {
   $(this).find(".overlay").css("opacity", 0);
   $(this).find(".itemText").css("opacity", 0)
 });
+
+$(".inputGroup input").focus(function() {
+  $(this).siblings().animate({
+    opacity: 1,
+    top: "-=25"
+  }, 500);
+})
+$(".inputGroup input").focusout(function() {
+  $(this).siblings().animate({
+    opacity: 0,
+    top: "+=25"
+  }, 200);
+})
+
+
+
+// $( "#book" ).animate({
+//    opacity: 0.25,
+//    left: "+=50",
+//    height: "toggle"
+//  }, 5000, function() {
+//    // Animation complete.
+//  });
